85472b71055a02fdcc99caad4304d36f05876d9d
Sam Edelsten <samedelsten1@gmail.com>
2021-09-24 17:37:14 +0100

    add shitty ball game

---
+++ apps/shitty-ball-game/js/class/DamageUpgrade.js
@@ -0,0 +1,16 @@
+import { game } from "../main.js";
+import { UpgradePanel } from "./UpgradePanel.js";
+export class DamageUpgrade extends UpgradePanel {
+    constructor(o) {
+        super(o);
+    }
+    get name() {
+        return 'Damage';
+    }
+    get currentLevel() {
+        return game.upgrades.ballDamage;
+    }
+    doUpgrade() {
+        game.upgrades.ballDamage++;
+    }
+}
---
+++ apps/shitty-ball-game/js/class/FloorUpgrade.js
@@ -0,0 +1,16 @@
+import { game } from "../main.js";
+import { UpgradePanel } from "./UpgradePanel.js";
+export class FloorUpgrade extends UpgradePanel {
+    constructor(o) {
+        super(o);
+    }
+    get name() {
+        return 'Floor';
+    }
+    get currentLevel() {
+        return game.upgrades.floorHealth;
+    }
+    doUpgrade() {
+        game.upgrades.floorHealth *= 2n;
+    }
+}
