4f43d4bd1dbd775bd9230f1f29080b1ffe7700fb
Sam Edelsten <samedelsten1@gmail.com>
2021-10-27 15:53:29 +0100

    fix constant refresh bug

--- docs/js/functions/openPage.js
+++ docs/js/functions/openPage.js
@@ -2,8 +2,10 @@ import { fadeBoxesOut, retractLines } from './animations.js';
 import { pages } from '../main.js';
 export async function openPage(page) {
     const pg = pages[page.replace('#', '')];
-    if (page !== 'blog')
-        location.search = '';
+    if (page !== 'blog') {
+        let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + location.hash;
+        window.history.pushState({ path: newurl }, '', newurl);
+    }
     await retractLines();
     await fadeBoxesOut();
     pg.render();
--- src/ts/functions/openPage.ts
+++ src/ts/functions/openPage.ts
@@ -5,11 +5,14 @@ import { pages } from '../main.js';
 export async function openPage(page) {
 	//grab page object
 	const pg = pages[page.replace('#', '')] as Page;
-	if (page !== 'blog') location.search='';
+	if (page !== 'blog') {
+		let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?' + location.hash;
+		window.history.pushState({path: newurl}, '', newurl);
+	}
 
 	await retractLines();
 	await fadeBoxesOut();
 	pg.render();
-
+	
 	return;
 }
