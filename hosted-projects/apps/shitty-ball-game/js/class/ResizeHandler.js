5647eb0db5fd6b02b5bc1e7a5d42350ca52c8a7d
samuel <samedelsten1@gmail.com>
2022-01-28 23:13:50 +0000

    move buildchain to dev repo

--- hosted-projects/apps/shitty-ball-game/js/class/ResizeHandler.js
+++
@@ -1,9 +0,0 @@
-export class ResizeHandler {
-    constructor() {
-        this.sizeRatio = 0;
-    }
-    set ratio(n) {
-        this.sizeRatio = n;
-    }
-}
-//# sourceMappingURL=ResizeHandler.js.map
--- src/ts/functions/wait.ts
+++
@@ -1,5 +0,0 @@
-export async function wait(ms:number) {
-    return new Promise(res=>{
-        setTimeout(()=>{res(0)},ms);
-    })
-}
